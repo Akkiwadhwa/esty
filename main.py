8940c83b2e7d73ebd51d0b46ccead693ab5cba06
Akkiwadhwa <114605480+Akkiwadhwa@users.noreply.github.com>
2022-11-13 19:50:07 +0530

    sql

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
-import concurrent.futures
-from bs4 import BeautifulSoup
-import requests
+from datetime import date
+from datetime import datetime
+from datetime import timedelta
+
+import mysql.connector
 import pandas as pd
+import requests
+from bs4 import BeautifulSoup
 from tqdm import tqdm
 
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="admin",
+    password="admin",
+    database="db"
+)
+mycursor = mydb.cursor(buffered=True)
 
-def etsy(x):
-    global s, dict1, shop_names, shop_name, soup1
-    print(x)
-    try:
-        r = requests.get(f"https://www.etsy.com/search/shops?ref=empty_redirect&order=alphabetical&page={x}")
-    except:
-        pass
-    else:
+
+def etsy():
+    s = "CREATE TABLE IF NOT EXISTS ETSY(Shop_Name TEXT,Sales_Data TEXT)"
+    mycursor.execute(s)
+    mydb.commit()
+
+    sql = "DELETE FROM ETSY"
+    mycursor.execute(sql)
+    mydb.commit()
+
+    sql = "INSERT INTO ETSY(Shop_Name,Sales_Data) VALUES( %s, %s)"
+    for x in range(1, 1250):
+        r = requests.get(f"https://www.etsy.com/in-en/search/shops?order=most_relevant&page={x}")
         data = r.text
         soup = BeautifulSoup(data, 'html.parser')
         shop_names = soup.find_all(class_="wt-text-title-01 wt-text-truncate")
@@ -23,26 +39,64 @@ def etsy(x):
                 data1 = r1.text
                 soup1 = BeautifulSoup(data1, 'html.parser')
                 sales_num = soup1.find(class_="wt-text-caption wt-no-wrap")
-                s = sales_num.text.split()[0]
+                sales = sales_num.text.split()[0]
             except:
-                dict1 = {
-                    "Shop name": shop_name,
-                    "Sales": "No sales data"
-                }
-                l1.append(dict1)
+                p_data = (shop_name, "No sales data")
             else:
-                dict1 = {
-                    "Shop name": shop_name,
-                    "Sales": s
-                }
-                l1.append(dict1)
-    return l1
+                p_data = (shop_name,
+                          sales)
+            mycursor.execute(sql, p_data)
+            mydb.commit()
+            print(mycursor.rowcount, "lines were inserted.")
 
 
-l1 = []
-total_pages = 1250
-with concurrent.futures.ThreadPoolExecutor(32) as executor:
-    results = list(tqdm(executor.map(etsy, range(1, total_pages + 1)), total=total_pages))
-    for i in results:
-        df = pd.DataFrame(i)
-        df.to_csv("data.csv")
+def track():
+    try:
+        global date, d
+        date = date.today()
+        d = f"{date.day}_{date.month}_{date.year}"
+        sql = f"alter table ETSY add column {d} TEXT"
+        mycursor.execute(sql)
+        mydb.commit()
+        print("a")
+    except:
+        print("A")
+        for x in range(1, 1250):
+            r = requests.get(f"https://www.etsy.com/in-en/search/shops?order=most_relevant&page={x}")
+            data = r.text
+            soup = BeautifulSoup(data, 'html.parser')
+            shop_names = soup.find_all(class_="wt-text-title-01 wt-text-truncate")
+            for i in shop_names:
+                shop_name = i.text
+                try:
+                    r1 = requests.get(f"https://www.etsy.com/in-en/shop/{shop_name}")
+                    data1 = r1.text
+                    soup1 = BeautifulSoup(data1, 'html.parser')
+                    sales_num = soup1.find(class_="wt-text-caption wt-no-wrap")
+                    sales = sales_num.text.split()[0]
+                except:
+                    pass
+                else:
+                    sql1 = f"insert into etsy (SELECT (COUNT(sales_data) - {sales}))"
+                    mycursor.execute(sql1)
+                    mydb.commit()
+    else:
+        pass
+
+
+track()
+# # taking input as the current date
+# # today() method is supported by date
+# # class in datetime module
+# Begindatestring = date.today()
+#
+# # print begin date
+# print("Beginning date")
+# print(Begindatestring)
+#
+# # calculating end date by adding 4 days
+# Enddate = Begindatestring - timedelta(days=4)
+#
+# # printing end date
+# print("Ending date")
+# print(Enddate)
